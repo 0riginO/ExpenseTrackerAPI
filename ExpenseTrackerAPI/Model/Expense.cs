4e9dc4709162044029d6584906bec4ecc0bb05b1
Deon <deonsadcopen@outlook.com>
2025-07-15 15:59:06 +0800

    Create Expense class with properties: - `Id` (int) - `Name` (string, default empty) - `Amount` (decimal) - `DateAdded` (DateTime)

---
+++ ExpenseTrackerAPI/Model/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpenseTrackerAPI.Model
+{
+	public class Expense
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public decimal Amount { get; set; }
+		public DateTime DateAdded { get; set; }
+	}
+
+}
